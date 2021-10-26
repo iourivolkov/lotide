110df28c4076308bfbaa3705a584c9d6a4985601
iourivolkov <volk.iouri@gmail.com>
2021-10-26 00:57:10 +0000

    created function to return first element of arry and check the assertion with previously created assert equal function

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+  } else if (actual !== expected) {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+   return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]));
+assertEqual(head([2]), 2);
+
+
+
