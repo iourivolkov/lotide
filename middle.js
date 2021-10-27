128352792b19efdfad3b4c0a5004ebf7f63ab6bd
iourivolkov <volk.iouri@gmail.com>
2021-10-27 18:33:28 +0000

    create function to return the middle elements from an array

---
+++ middle.js
@@ -0,0 +1,57 @@
+// assertEqual 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+  } else if (actual !== expected) {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays
+const eqArrays = function(array1, array2){
+  if(array1.length !== array2.length){
+    return false;
+  } else {
+    for(let i = 0; i < array1.length; i++){
+      if(array1[i] !== array2[i]){
+      return false; 
+     }
+    }
+    return true;
+  }
+};
+
+// middle
+const middle = (arr) => {
+  let i = 0; 
+  let result = [];
+
+  if(arr.length == 1 || arr.length == 2 || arr.length == 0){ // if arr contains 1 elem, 2 elems, or no elems - return empty array
+    return [];
+
+  } else if (arr.length % 2 == 1) { // if array is odd (remainder) - return the middle elem (single)
+    i = (arr.length - 1) /2;
+    result.push(arr[i]);
+    return result;
+
+  } else if (arr.length % 2 == 0) { // if array is even (no remainder) - return the two middle elems
+    i = arr.length / 2;
+    result.push(arr[i - 1], arr[i]);
+    return result;
+   }
+};
+
+// TEST CODE
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])) // => []
+
+console.log(middle([1, 2, 3])); // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+
+
+
