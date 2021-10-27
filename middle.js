2a369fadcba33aeb004b0b016f98c3626959d638
iourivolkov <volk.iouri@gmail.com>
2021-10-27 18:40:28 +0000

    create function to return middle elements of array

--- middle.js
+++ middle.js
@@ -41,16 +41,9 @@ const middle = (arr) => {
    }
 };
 
-// TEST CODE
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])) // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
 
 
 
