bf396be06302862f575c74a5cb59f7d967e14ba3
iourivolkov <volk.iouri@gmail.com>
2021-11-01 22:39:05 +0000

    refactor eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-
+const assertEqual = require('./assertEqual')
 
 const eqArrays = function(array1, array2){
   if(array1.length !== array2.length){
@@ -22,20 +13,9 @@ const eqArrays = function(array1, array2){
   }
 };
 
+module.exports = eqArrays;
 
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [2, 3, 4]), true); 
-assertEqual(eqArrays([""], [""]), true); 
-assertEqual(eqArrays([3], [3]), true); 
-assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), true); 
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
 
 
 
