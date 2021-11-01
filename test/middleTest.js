3f4b737e1478f3d32e74c97da444546276cf9429
iourivolkov <volk.iouri@gmail.com>
2021-11-01 23:36:47 +0000

    tests for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const assertEqual = require('../assertEqual');
+const middle = require('../middle');
+
+console.log(middle([1,2,3,4,5]));
+console.log(middle([1,2,3,6,5,4]));
+
+
+
+
+
