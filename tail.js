91c89642beb23cdc4f55c9eadf6d9141d1bcb084
iourivolkov <volk.iouri@gmail.com>
2021-11-01 22:31:18 +0000

    refactor tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let tailArr = [];
@@ -17,18 +11,5 @@ const tail = function(array) {
   return tailArr;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(tail(words)); // returns ['"Lighthouse","Labs"]
-assertEqual(words.length, 3); // assertion passed! 3 === 3
+module.exports = tail;
 
-// An array with only one element should yield an empty array for its tail
-// An empty array should yield an empty array for its tail
-
-const testCase = [1];
-tail(testCase);
-console.log(tail(testCase)); // yields empty
-
-const testCase2 = [];
-console.log(tail(testCase2)); // yields empty array
